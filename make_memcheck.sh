4c9bfc2cc733323969b51d5479833b655acb1455
climbfuji <dom.heinzeller@icloud.com>
2018-08-31 09:32:53 -0600

    Rename platform ubuntu to linux

--- make_memcheck.sh
+++ make_memcheck.sh
@@ -33,7 +33,7 @@ THIS_FILE=$(basename "$0" )
 #--------------------------------------------------------------
 # Define available options
 #--------------------------------------------------------------
-validsystems=( theia cheyenne macosx ubuntu )
+validsystems=( theia cheyenne macosx linux )
 validcompilers=( intel pgi gnu )
 
 #--------------------------------------------------------------
